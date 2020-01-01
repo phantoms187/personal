537d212fb107178e46c5e315d8e316a700142cda
Trip <warrend187@gmail.com>
2020-01-01 08:02:01 +0000

    Change to Node server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 4000;
+
+app.use(express.static('Public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+//app.use(express.static(path.join("client", "skatejoring", "build",)));
+
+app.get('/*', function(req, res) {
+    res.sendFile(__dirname +"/index.html");
+  });
+  
+app.listen(PORT, function(){
+  console.log('Server is running on Port:', PORT);
+});
